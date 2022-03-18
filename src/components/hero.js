534c5fc179f681b054dd377b8615c007a7dcdc55
Tyler <tjrankin572@gmail.com>
2022-03-17 21:47:49 -0600

    Baby time

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import  "../styles/global.css"
+import "../styles/ryan_madi.css"
 
 import { Link } from "gatsby"
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -28,17 +28,26 @@ const MissionPartner2 = () => (
     </section>
 
     <section className="our-story section-alignment" id="our-story">
-      <div className="wrapper">
+      <div className="our-story-wrapper">
         <h2>Our Story</h2>
         <StaticImage src="../images/heart_vector.png" className="icon" objectFit="fill" width={500}/>
-        <p>We met, he was hot... That's about it. Also his brother is pretty cool.</p>
+        <p>I was at a friends house back in June of 2020. We were chatting, as girls do, about dates. 
+          My friend’s sister was telling us about a date she had recently been on and explaining why their personalities 
+          and values didn’t line up. Only everything she brought up was something I was looking for. 
+          I jokingly told her to give him my number…and she did.</p>
+          <br></br>
+          <p>After a year of knowing each other we officially started dating and then after 8 months of dating, 
+          Ryan asked me to marry him on New Years Eve at a nerdy Star Wars party. 
+          It was perfect for us.And now here we are.</p>
+          <br></br> 
+          <p>We hope you will join us for our special day as we start the rest of our lives together!</p>
       </div>
       
     </section>
     <section className="when-and-where section-alignment" id="when-and-where">
       
 
-      <div className="rsvp-wrapper">
+      <div className="when-and-where-wrapper">
         <h2>When & Where</h2>
         <StaticImage src="../images/map_icon_vector.png" objectFit="fill" className="icon" width={500}/>
         <p>Treasure Valley Baptist Church</p>
@@ -63,7 +72,14 @@ const MissionPartner2 = () => (
 
         <h2>Contact</h2>
         <p>Let us know if you run into any issues!</p>
-        <p>Phone: 555-555-5555</p>        
+
+        <h3>Ryan Rankin</h3>
+        <p>Phone: (208) 484-2688</p>  
+        <p>Email: ryanwillrankin@gmail.com</p>      
+
+        <h3>Madison Barno</h3>
+        <p>Phone: (775) 530-2292</p>  
+        <p>Email: madison.barno@gmail.com</p>      
 
       </div>
 
@@ -74,17 +90,27 @@ const MissionPartner2 = () => (
       <div className="white-wrapper">
         <h2>Registry</h2>
         <StaticImage src="../images/drawing_vector.png" className="icon" width={500}/>
-        <p>Buy Us Things</p>
+        <br></br>
         <a href="https://www.bedbathandbeyond.com/store/giftregistry/viewregistryguest/551170972?eventType=Wedding&fbclid=IwAR1GpSRW3Yaktp3B5Q6vc2-RJUgQx4izSDp57TucRBr1w5h1iBLRIAOpVs8" target="_blank" class="flex-item btn btn-dark bottom-margin-b-20x">Bed Bath & Beyond</a>
       </div>
 
     </section>
 
     <section className="verse-section section-alignment" id="verse">
-      <div className="wrapper">
-        <h2>Bible Reference</h2>
+      <div className="verse-wrapper">
+        <h2>Favorite Bible Verses</h2>
+        <br></br>
         <StaticImage src="../images/cross_vector.png" className="icon" width={100}/>
-        <p>The Verse text...hello there</p>
+        <br></br>
+        <h3>Ryan's Favorite</h3>
+        <h3>Philippians 4:6-7</h3>
+        <p>Do not be anxious about anything, but in every situation, by prayer and petition, with thanksgiving, 
+          present your requests to God. And the peace of God, which transcends all understanding, 
+          will guard your hearts and your minds in Christ Jesus.</p>
+        <br></br>
+        <h3>Madi's Favorite</h3>
+        <h3>Luke 1:37</h3>
+        <p> For with God nothing shall be impossible.</p>
       </div>
     </section>
 
